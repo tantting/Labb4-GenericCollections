4a2f2e77ab04643c54a21a22b1a556625bac511f
tantting <jennyann.hayward82@gmail.com>
2024-11-14 12:06:29 +0100

    Simplified set-up - no user meny to stear actions

--- Program.cs
+++ Program.cs
@@ -18,51 +18,91 @@ class Program
         restaurant.AddtoMenu(menuItem2);
         restaurant.AddtoMenu(menuItem3);
         restaurant.AddtoMenu(menuItem4);
-
-        Console.WriteLine("Vad vill du göra?\n" +
-                          "1. Skriva ut menyn\n" +
-                          "2. Skapa ny order\n" +
-                          "3. Visa aktuella ordrar\n" +
-                          "4. Visa analet ordrar i kö\n" +
-                          "5. Visa nästa order i kö\n" +
-                          "6. Hantera en order\n" +
-                          "7. Stänga restaurangen för idag!");
-
-        string choice = Console.ReadLine();
-
-        bool runRestaurant = true;
-
-        while (runRestaurant)
+        
+        //Printing the meny to the consol. 
+        restaurant.ShowMenu();
+        
+        //Create a list of menuItems that is ordered. 
+        List<MenuItem> orderItems1 = new List<MenuItem>()
         {
-            switch (choice)
-            {
-                case "1":
-                    restaurant.ShowMenu();
-                    break; 
-                case "2":
-                    List<MenuItem> menu = restaurant.Menu; 
-                    
-                    break; 
-                case "3":
-                    Console.WriteLine("Visa aktuella ordrar");
-                    break; 
-                case "4":
-                    Console.WriteLine("Visa antalet ordrar i kö");
-                    break; 
-                case "5":
-                    Console.WriteLine("Visa nästa order i kö");
-                    break; 
-                case "6":
-                    Console.WriteLine("Hantera en order");
-                    break; 
-                case "7":
-                    Console.WriteLine("Tack för idag! Bra jobbat!");
-                    runRestaurant = false; 
-                    break; 
-            }
-        }
+            restaurant.Menu[0], restaurant.Menu[0], restaurant.Menu[3],
+        };
+        //Create a new order object of the chosen menu items. 
+        Order order1 = new Order(orderItems1, 3);
+        //Creata and lof the order
+        restaurant.CreateOrder(order1);
+        
+        //Do the same procedure two more times 
+        List<MenuItem> orderItems2 = new List<MenuItem>()
+        {
+            restaurant.Menu[0], restaurant.Menu[1], restaurant.Menu[1],
+        }; 
+        Order order2 = new Order(orderItems2, 7);
+        restaurant.CreateOrder(order2);
+        
+        List<MenuItem> orderItems3 = new List<MenuItem>()
+        {
+            restaurant.Menu[1], restaurant.Menu[2], restaurant.Menu[3],
+        };
+        Order order3 = new Order(orderItems3, 1); 
+        restaurant.CreateOrder(order3);
+        
+        //Print out all orders in the queue. 
+        restaurant.ShowOrders();
+        
+        //Print out the amount of orders in the queue.
+        restaurant.ShowOrderCount();
+        
+        //Print out the next order in the queue
+        restaurant.ShowNextOrder();
+        
+        //Handle an order
+        restaurant.HandleOrder();
+        
+        //Show the amount of orders i queue. 
+        restaurant.ShowOrderCount();
+        
+        //Add one more order to the queue
+        List<MenuItem> orderItems4 = new List<MenuItem>()
+        {
+            restaurant.Menu[1], restaurant.Menu[2]
+        };
+        Order order4 = new Order(orderItems3, 1); 
+        restaurant.CreateOrder(order4);
+        
+        //Show the amount of orders i queue. 
+        restaurant.ShowOrderCount();
+        
+        //Handle an order
+        restaurant.HandleOrder();
+        
+        //Handle one more orders
+        restaurant.HandleOrder();
+        
+        //Show the amount of orders i queue. 
+        restaurant.ShowOrderCount();
+        
+        //Print out the next order in the queue
+        restaurant.ShowNextOrder();
+        
+        //Handle an order
+        restaurant.HandleOrder();
+        
+        //Show the amount of orders i queue. 
+        restaurant.ShowOrderCount();
         
         
         
+        
+        
+        
+        
+        
+        
+        
+
+
+
+
     }
 }
